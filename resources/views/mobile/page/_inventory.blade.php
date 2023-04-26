6cea65b31b9c593cb3c92321cac4336e13f26cfd
muarif27 <muarif130912@gmail.com>
2023-04-26 19:25:27 +0700

    Update Tampilan Modal Data Inventory Secara Detail

--- resources/views/mobile/page/_inventory.blade.php
+++ resources/views/mobile/page/_inventory.blade.php
@@ -3,11 +3,11 @@
 <div class="page-content">
     @foreach($DataRuangan as $dr)
     @foreach(DB::table('penempatan_barang')->where('ruangan',$dr->id)->select('kode_barang','nama_barang')->groupBy('kode_barang','nama_barang')->get() as $db)
-    <div class="card card-style bg-16" data-card-height="130">
+    <div class="card card-style bg-16" data-card-height="150">
         <div class="content card-top text-start">
             <div class="d-flex">
                 <div>
-                    <a href="#" data-menu="menu-success-2-{{$db->kode_barang}}"><img src="data:image/png;base64,{{DNS2D::getBarcodePNG($db->kode_barang, 'qrcode')}}" style="background-color:rgb(255,255,255); padding:4px;" width="90"></a>
+                    <a href="#" data-menu="menu-success-2-{{$db->kode_barang}}"><img src="data:image/png;base64,{{DNS2D::getBarcodePNG($db->kode_barang, 'qrcode')}}" style="background-color:rgb(255,255,255); padding:4px;" width="100"></a>
                 </div>
                 <p class="text-white" style="padding-left: 6px;">
                     Nama Ruangan : {{ $dr->nama_ruangan }}</br>
@@ -15,9 +15,9 @@
                     Jumlah Barang : <?php $count_bg = DB::table('penempatan_barang')->where('ruangan', $dr->id)->where('kode_barang', $db->kode_barang)->count() ?> {{$count_bg}} Barang</br>
                     Kode Barang : {{ $db->kode_barang}}</br>
                 </p>
-                <div class="card-top text-end ">
-                    <div class="me-3 color-white">
-                        <a href="#" data-menu="menu-success-2-1-{{$db->kode_barang}}" class="btn btn-xxs rounded-m text-uppercase font-700 shadow-s bg-blue-light">Lihat Barang</a>
+                <div class="card-bottom text-end">
+                    <div class="me-1 color-white">
+                        <a href="#" data-menu="menu-success-2-1-{{$db->kode_barang}}" class="btn btn-xxs rounded-m text-uppercase font-500 shadow-s bg-blue-light">Lihat Barang</a>
                     </div>
                 </div>
 
@@ -39,8 +39,8 @@
 @endforeach
 @foreach($DataRuangan as $dr)
 @foreach(DB::table('penempatan_barang')->where('ruangan',$dr->id)->select('kode_barang','nama_barang')->groupBy('kode_barang','nama_barang')->get() as $db)
-<div id="menu-success-2-1-{{$db->kode_barang}}" class="menu menu-box-modal rounded-m" style="padding-left: 4px; padding-right:4px;" data-menu-height=" 350" data-menu-width="400">
-    <h6 class=" mt-2 font-700 color-white" style="padding-left:4px">Nama Barang : {{ $db->nama_barang }}</h6>
+<div id="menu-success-2-1-{{$db->kode_barang}}" class="menu menu-box-modal rounded-m" style="padding-left: 4px; padding-right:4px;" data-menu-width="400">
+    <h6 class=" mt-2 font-700" style="padding-left:4px">Nama Barang : {{ $db->nama_barang }} / {{$db->kode_barang}}</h6>
     <table class="table table-borderless text-center rounded-sm shadow-l" style="overflow: hidden;">
         <thead>
             <tr>
@@ -51,12 +51,15 @@
             </tr>
         </thead>
         <tbody>
+            <?php $no = 1; ?>
+            @foreach(DB::table('penempatan_barang')->where('ruangan',$dr->id)->where('kode_barang',$db->kode_barang)->get() as $br)
             <tr class="bg-blue-light color-gray-light">
-                <th scope="row">Apple</th>
-                <td>iPhone</td>
-                <td>iOS</td>
-                <td>Yes</td>
+                <th scope="row">{{$no++}}</th>
+                <td>{{$br->merk}}</td>
+                <td>{{$br->no_urut}}</td>
+                <td>{{$br->tahun_perolehan}}</td>
             </tr>
+            @endforeach
         </tbody>
     </table>
 </div>
